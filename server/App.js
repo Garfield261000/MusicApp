90cc950af0a146f5c9697c071a8f650e3d3a456b
Garfield261000 <rishabhpandram26@gmail.com>
2023-03-12 14:20:49 +0530

    user auth

--- server/App.js
+++ server/App.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+require ('dotenv'). config ();
 
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+// app.use(cors({origin : true}));
 
 app.get('/', function (req, res) {
   return res.json("hi..")
 });
 
+//user authentication route
+const userRoute = require("./routes/auth");
+app.use("/api/user/",userRoute);
 
+mongoose.connect(process.env.DB_URL,{useNewUrlParser : true});
+mongoose.connection
+.once("open", ()=> console.log("Connected"))
+.on("error", (error)=> console.log(`Error: ${error}`))
 
 app.listen(4000, ()=> console.log("Listening to port 4000"));
